ef12d388b7df47bd1367ea8f04aa37c3a8a0930e
vefa çağlar <noxfide@gmail.com>
2026-03-06 23:32:55 +0300

    add run / walk speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed = 450f;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 8f;
 
     private CharacterController controller;
+    private Quaternion targetRotation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,12 +20,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(input * Time.deltaTime * 5);
+        // controller.Move(input * Time.deltaTime * 5);
 
-        if(input != Vector3.zero)
+        if (input != Vector3.zero)
         {
-            var targetRotation = Quaternion.LookRotation(input);
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf
+                .MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10);
         }
+
+        Vector3 motion = input;
+        motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? 0.7f : 1f; // if moving diagonally, reduce speed to 70% to prevent faster movement
+        motion *= Input.GetButton("Run") ? runSpeed : walkSpeed; // if run button is held, use run speed, otherwise use walk speed
+        motion += Vector3.up * -8; // add gravity
+
+        controller.Move(motion * Time.deltaTime);
     }
 }
